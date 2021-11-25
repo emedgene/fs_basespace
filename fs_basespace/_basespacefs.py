e9ce8f31ac90683c1677ead9d3b662e97b8eb717
Amit Ezra <amit@emedgene.com>
2021-11-25 09:13:30 +0200

    Move all BasespaceAPI to a factory so we only call them once

--- fs_basespace/_basespacefs.py
+++ fs_basespace/_basespacefs.py
@@ -16,8 +16,8 @@ from fs.path import normpath
 from fs.path import relpath
 from smart_open.http import SeekableBufferedInputBase
 
-from BaseSpacePy.api.BaseSpaceAPI import BaseSpaceAPI
 
+from .api_factory import BasespaceApiFactory
 from .basespace_context import FileContext
 from .basespace_context import CategoryContext
 from .basespace_context import get_last_direct_context
@@ -81,12 +81,9 @@ class BASESPACEFS(FS):
         logger.debug('BaseSpaceFs is created')
 
     @property
-    def basespace(self):
+    def basespace(self) -> BasespaceApiFactory:
         if not hasattr(self._tlocal, "basespace"):
-            self._tlocal.basespace = BaseSpaceAPI(self.client_id,
-                                                  self.client_secret,
-                                                  self.basespace_server,
-                                                  AccessToken=self.access_token)
+            self._tlocal.basespace = BasespaceApiFactory(self.client_id, self.client_secret, self.basespace_server, self.access_token)
         return self._tlocal.basespace
 
     def __repr__(self):
